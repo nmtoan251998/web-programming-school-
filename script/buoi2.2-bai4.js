9172f47500fb22bf90e0e991d93623f44abb33d4
nmtoan251998@gmail.com <nmtoan251998@gmail.com>
2019-03-13 18:42:31 +0700

    initialize

--- script/buoi2.2-bai4.js
+++ script/buoi2.2-bai4.js
@@ -2,31 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const a = document.getElementById("a"),
           b = document.getElementById("b"),  
           c = document.getElementById("c"),
-          btn = document.getElementById("solve-btn");
+          btn = document.getElementById("solve-btn"),
+          deltaHTML = document.getElementById("delta"),
+          messageHTML = document.getElementById("message"),
+          x1HTML = document.getElementById("x1"),
+          x2HTML = document.getElementById("x2");
     
     const solution = (a, b, c) => {
-        const delta = Math.pow(b, 2) - 4 * a * c;
-        const result = {
+        const delta = Math.pow(b, 2) - (4 * a * c);        
+        
+        let result = {
             x1: undefined,
-            x2: undefined
+            x2: undefined,
+            message: ""
         };        
 
         if((a + b + c) === 0) {
-            result.x1 = 1;
-            result.x2 = c / a;
-        } else if((a - b + c) === 0) {
-            result.x1 = -1;
-            result.x2 = -(c / a);
+            result = { x1: 1, x2: c/a, message: "Phương trình có 2 nghiệm:"}
+        } else if((a - b + c) === 0) {            
+            result = { x1: 1, x2: -(c / a), message: "Phương trình có 2 nghiệm:"}
         }
 
         if(delta < 0) {
-            result.x1 = null;
-            result.x2 = null;
+            result = { x1: null, x2: null, message: "Phương trình vô nghiệm:" }            
         } else if(delta === 0) {
-            result.x1 = result.x2 = -(b / 2 * a);
-        } else if(delta >= 0) {
-            result.x1 = (-(b) + Math.sqrt(delta) ) / 2 * a;
-            result.x2 = (-(b) - Math.sqrt(delta) ) / 2 * a;
+            result = { x1: -(b / 2 * a), x2: -(b / 2 * a), message: "Phương trình có nghiệm kép:" }            
+        } else if(delta > 0) {
+            result = { x1: ((-b) + Math.sqrt(delta)) / (2 * a), x2: ((-b) - Math.sqrt(delta)) / (2 * a), message: "Phương trình có 2 nghiệm riêng lẻ:" }                        
         } 
                         
         return {result, delta};
@@ -35,9 +37,9 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", (el) => {
         el.preventDefault();
         
-        let aValue = parseInt(a.value),
-        bValue = parseInt(b.value),
-        cValue = parseInt(c.value);
+        const aValue = Math.abs(parseInt(a.value)),
+        bValue = Math.abs(parseInt(b.value)),
+        cValue = Math.abs(parseInt(c.value));        
         
         if(!a.value || parseInt(a.value) === 0) {
             alert("A phải khác 0");
@@ -46,6 +48,9 @@ document.addEventListener("DOMContentLoaded", () => {
         
         const {result, delta} = solution(aValue, bValue, cValue);
             
-        alert("delta = "+delta +"\nx1 = " +result.x1 +"\nx2 = "+result.x2)
+        deltaHTML.innerHTML = delta;
+        messageHTML.innerHTML = result.message;      
+        x1HTML.innerHTML = result.x1;
+        x2HTML.innerHTML = result.x2;        
     })
 })
---
+++ script/buoi2.2-bai5.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const name = document.getElementById("name"),
+          sex = document.getElementById("sex"),
+          email = document.getElementById("email"),
+          dob = document.getElementById("dob"),
+          city = document.getElementById("city"),
+          add = document.getElementById("add"),
+          region = document.getElementById("region"),
+          zip = document.getElementById("zip"),
+          btnFinished = document.getElementById("btn-finished"),
+          btnClear = document.getElementById("btn-clear");
+
+    const clearInput = () => {
+        name.value = "";
+        sex.value = "";
+        email.value = "";
+        dob.value = "";
+        city.value = "";
+        add.value = "";
+        region.value = "";
+        zip.value = "";    
+    }
+
+    btnClear.addEventListener("click", (el) => {
+        el.preventDefault();
+
+        clearInput();
+    })
+
+    btnFinished.addEventListener("click", (el) => {
+        el.preventDefault();
+
+        if(name.value === "" || sex.value === "" || email.value === "" ||
+           dob.value === "" || city.value === "" || add.value === "" ||
+           region.value === "" || zip.value === "") {
+               alert("Input value for the text boxes");
+               return;
+           }
+
+        alert("Submitted");
+
+        clearInput();
+    })
+})
